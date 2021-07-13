3008f03665b1bf7d2bff86996d0e0646530a735e
Ayush Mayur <am3nz@virginia.edu>
2021-07-13 15:50:47 -0400

    test

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,12 @@
-import pandas as pd
+#import pandas as pd
 
 def lambda_handler(event, context):
-    d = {'col1': [1,2], 'col2': [3,4]}
-    df = pd.DataFrame(data=d)
-    print(df)
-    print('Done x1.2')
+    print("hello friends")
+
+
+
+# def lambda_handler(event, context):
+#     d = {'col1': [1,2], 'col2': [3,4]}
+#     df = pd.DataFrame(data=d)
+#     print(df)
+#     print('Done x1.2')
